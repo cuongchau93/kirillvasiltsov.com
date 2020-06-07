560b1a7a7a9d3ab5594a433fb77ccd0c4f6a76b2
jlkiri <interalia.dev@gmail.com>
2020-06-08 00:15:04 +0900

    Create toggle

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -33,6 +33,75 @@
   });
 </script>
 
+<style>
+  .theme-toggle {
+    cursor: pointer;
+  }
+
+  .theme-toggle input {
+    display: none;
+  }
+
+  .theme-toggle input + div {
+    border-radius: 50%;
+    width: 26px;
+    height: 26px;
+    position: relative;
+    box-shadow: inset 10px -10px 0 0 var(--auxtext);
+    transform: scale(1) rotate(-2deg);
+    transition: box-shadow 0.5s ease 0s, transform 0.4s ease 0.1s;
+  }
+
+  .theme-toggle input + div:before {
+    content: "";
+    width: inherit;
+    height: inherit;
+    border-radius: inherit;
+    position: absolute;
+    left: 0;
+    top: 0;
+    transition: background 0.3s ease;
+  }
+
+  .theme-toggle input + div:after {
+    content: "";
+    width: 6px;
+    height: 6px;
+    border-radius: 50%;
+    margin: -3px 0 0 -3px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    box-shadow: 0 -14px 0 var(--auxtext), 0 14px 0 var(--auxtext),
+      14px 0 0 var(--auxtext), -14px 0 0 var(--auxtext),
+      10px 10px 0 var(--auxtext), -10px 10px 0 var(--auxtext),
+      10px -10px 0 var(--auxtext), -10px -10px 0 var(--auxtext);
+    transform: scale(0);
+    transition: all 0.3s ease;
+  }
+
+  .theme-toggle input:checked + div {
+    box-shadow: inset 32px -32px 0 0 var(--auxtext);
+    transform: scale(0.5) rotate(0deg);
+    transition: transform 0.3s ease 0.1s, box-shadow 0.2s ease 0s;
+  }
+
+  .theme-toggle input:checked + div:before {
+    background: var(--auxtext);
+    transition: background 0.3s ease 0.1s;
+  }
+
+  .theme-toggle input:checked + div:after {
+    transform: scale(1.5);
+    transition: transform 0.5s ease 0.15s;
+  }
+
+  .top-right {
+    top: 1.4rem;
+    right: 2rem;
+  }
+</style>
+
 <svelte:head>
   <script>
     (() => {
@@ -48,7 +117,10 @@
   </script>
 </svelte:head>
 
-<div class="relative text-text min-h-screen bg-auxbg">
-  <button on:click={toggleMode}>Toggle theme</button>
+<div class="relative text-text min-h-screen bg-bg">
+  <label class="absolute top-right theme-toggle">
+    <input checked={$mode === 'dark'} type="checkbox" on:change={toggleMode} />
+    <div />
+  </label>
   <slot />
 </div>
