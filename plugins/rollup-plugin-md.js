6db01f415b522ef6d1de1c6b8b65487bf497d896
jlkiri <interalia.dev@gmail.com>
2020-06-10 23:18:18 +0900

    Syntax highlighting

--- plugins/rollup-plugin-md.js
+++ plugins/rollup-plugin-md.js
@@ -2,31 +2,57 @@ import marked from "marked";
 import { createFilter } from "rollup-pluginutils";
 import matter from "gray-matter";
 import path from "path";
+import shiki from "shiki";
 
 export default function md(
   options = { include: ["**/*.md"], marked: { smartypants: true } }
 ) {
   const filter = createFilter(options.include, options.exclude);
 
-  if (options.marked) {
-    marked.setOptions(options.marked);
+  let theme = options.theme || "nord";
+  let shikiTheme;
+
+  try {
+    shikiTheme = shiki.getTheme(theme);
+  } catch (_) {
+    try {
+      shikiTheme = shiki.loadTheme(theme);
+    } catch (_) {
+      throw new Error("Unable to load theme: " + theme);
+    }
   }
 
   return {
     name: "md",
 
-    transform(md, id) {
+    async transform(md, id) {
       if (!filter(id)) return null;
+
+      const shikiHightlighter = await shiki.getHighlighter({
+        theme: shikiTheme,
+      });
+
+      const highlighter = (code, lang, callback) => {
+        const result = shikiHightlighter.codeToHtml(code, lang);
+        callback(null, result);
+      };
+
+      if (options.marked) {
+        marked.setOptions({ ...options.marked, highlight: highlighter });
+      }
 
       const slug = path.basename(id, ".md");
       const { data: frontmatter, content } = matter(md);
-      const html = marked(content);
-      const data = { html, frontmatter, slug };
 
-      return {
-        code: `export default ${JSON.stringify(data)};`,
-        map: { mappings: "" },
-      };
+      return new Promise((resolve, reject) => {
+        marked(content, null, (_, result) => {
+          const data = { html: result, frontmatter, slug };
+          resolve({
+            code: `export default ${JSON.stringify(data)};`,
+            map: { mappings: "" },
+          });
+        });
+      });
     },
   };
 }
